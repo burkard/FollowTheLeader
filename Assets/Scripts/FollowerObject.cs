3dcb606dfc0c5b89e008c81f16c35b4eabdcdd31
Burkard <fabioburkard@gmail.com>
2019-05-09 10:53:49 -0300

    Initial commit

---
+++ Assets/Scripts/FollowerObject.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowerObject : MonoBehaviour
+{
+    public GameObject target;
+
+    //[Tooltip("Seconds to wait before start moving")]
+    //public float waitingTime;
+
+    [Tooltip("Moving speed, in meters per second")]
+    public float movingSpeed;
+    [Tooltip("The higher the speed, the quicker they turn to the target")]
+    public float turningSpeed;
+
+    private Vector3 relativePos;
+    private Quaternion rotation;
+
+    private List<Vector3> desiredPositions;
+    private bool isFollowing;
+
+    private static float startFollowingSqrDistance, stayAwaySqrDistance, currentSqrDistance, sqrMoveThreshold;
+
+    void Start()
+    {
+        if (target == null || movingSpeed <= 0f || turningSpeed == 0f) Debug.LogError("Error - please set target and speed for " + this.name);
+        desiredPositions = new List<Vector3>();
+        isFollowing = false;
+
+        if (startFollowingSqrDistance == 0f) startFollowingSqrDistance = Vector3.SqrMagnitude(
+                new Vector3(target.GetComponent<LeaderObject>().startFollowingDistance, 0f, 0f)
+            );
+
+        if (stayAwaySqrDistance == 0f) stayAwaySqrDistance = Vector3.SqrMagnitude(
+                new Vector3(target.GetComponent<LeaderObject>().stayAwayDistance, 0f, 0f)
+            );
+
+        if (sqrMoveThreshold == 0f) sqrMoveThreshold = Vector3.SqrMagnitude(
+                new Vector3(target.GetComponent<LeaderObject>().moveThreshold, 0f, 0f)
+            );
+    }
+
+    void Update()
+    {
+        currentSqrDistance = Vector3.SqrMagnitude(target.transform.position - transform.position);
+        if (currentSqrDistance <= startFollowingSqrDistance)
+        {
+            isFollowing = true;
+            if (currentSqrDistance > stayAwaySqrDistance) MoveToTarget();
+        }
+        else
+        {
+            isFollowing = false;
+        }
+
+        if (isFollowing)
+        {
+            LookToTarget();
+            RecordTargetPosition();
+        }
+        else
+        {
+            desiredPositions.Clear();
+        }
+
+
+    }
+
+    void RecordTargetPosition()
+    {
+        // record target's position ONLY IF target has moved beyond the threshold:
+        if (desiredPositions.Count == 0) desiredPositions.Add(target.transform.position);
+
+        if (desiredPositions.Count > 0 && Vector3.SqrMagnitude(desiredPositions[desiredPositions.Count - 1] - target.transform.position) >= sqrMoveThreshold)
+            desiredPositions.Add(target.transform.position);
+    }
+
+    void LookToTarget()
+    {
+        relativePos = target.transform.position - transform.position;
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(relativePos, Vector3.up), turningSpeed * Time.deltaTime);
+    }
+
+    void MoveToTarget()
+    {
+        if (desiredPositions.Count > 0)
+        {
+            relativePos = desiredPositions[0] - transform.position;
+
+            if (relativePos.sqrMagnitude <= 0.1f)
+            {
+                desiredPositions.RemoveAt(0);
+            }
+            
+            transform.position += relativePos.normalized * movingSpeed * Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/Scripts/LeaderObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeaderObject : MonoBehaviour
+{
+    [Tooltip("when objects reach this distance, they stop")]
+    public float stayAwayDistance;
+
+    [Tooltip("when objects are inside this range, they follow me")]
+    public float startFollowingDistance;
+
+    [Tooltip("Minimum travelled distance to record position, in meters")]
+    public float moveThreshold; // the followers will keep it's own records
+
+    void Start()
+    {
+        if (stayAwayDistance == 0f || startFollowingDistance == 0f || moveThreshold == 0f) Debug.LogWarning("If you don't set a startFollowingDistance (and stayAway) for " + this.gameObject.name + ", the followers will start to follow IMEDIATELLY! Is this what you want?");
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, stayAwayDistance);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, startFollowingDistance);
+    }
+}
